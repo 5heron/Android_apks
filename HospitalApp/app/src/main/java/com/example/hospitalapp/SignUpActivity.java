48b0caa9cc519e377a83e0e8f06af2c702509f8a
Sheron <sheronroy2003@gmail.com>
2024-12-16 21:05:21 +0530

    first first commit

---
+++ HospitalApp/app/src/main/java/com/example/hospitalapp/SignUpActivity.java
@@ -0,0 +1,30 @@
+package com.example.hospitalapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SignUpActivity extends AppCompatActivity {
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+        b1=(Button)findViewById(R.id.loginbut1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ob = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob);
+            }
+        });
+    }
+}
